1f001d68da9c13a4c599a384ed44650cc37706be
wolfgangB33r <wolfgang@smartlab.at>
2019-06-14 07:27:25 +0200

    added config endpoint

--- README.md
+++ README.md
@@ -11,4 +11,33 @@ Purpose of this service is to simulate specific anomaly situations, such as:
 Therefore, this generic service can be used to build all kinds of variable demo 
 situations with either flat or deep call trees.
 
+The service listenes on following HTTP resource pathes:
+- GET '/' normal page return
+- POST '/config' service receives anomaly config as a HTTP POST message with a JSON config payload that is defined below.
+
+
+## Config JSON body
+
+{
+  "error" : {
+    
+  },
+  "slowdown" : {
+    
+  },
+  "crash" : {
+  
+  },
+  "cpu" : {
+    
+  },
+  "memory" : {
+    
+  },
+  "callees" : [
+    { "adr" : "http://www.example.com", "count" : 10 },
+    { "adr" : "http://www.orf.at", "count" : 3 }
+  ]
+}
+
 
--- service.go
+++ service.go
@@ -2,66 +2,35 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"net/http"
 	"strings"
-	"time"
 )
 
-var slowrequests = 0
-var errorrequests = 0
+func receiveConfig(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		w.WriteHeader(http.StatusNoContent)
+
+	default:
+		fmt.Fprintf(w, "Sorry, only POST method is supported.")
+
+		//w.Write([]byte("500 - Something bad happened!"))
+	}
+	defer r.Body.Close()
+}
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
-	today := time.Now()
-	fmt.Println("Hour: ", today.Hour())
-
-	//
-	slowdown := r.URL.Query().Get("slowdown")
-	if slowdown != "" {
-		slowrequests = 200
-	}
-
-	//
-	error := r.URL.Query().Get("error")
-	if error != "" {
-		errorrequests = 200
-	}
-
-	// artificial slowdown in case of a slowdown problem
-	if slowrequests > 0 {
-		time.Sleep(200 * time.Millisecond)
-		// start some CPU/mem intensive operation
-		m1 := [1000][1000]int{}
-		for i := 0; i < 1000; i++ {
-			for j := 0; j < 1000; j++ {
-				m1[i][j] = rand.Int()
-			}
-		}
-
-		// End of CPU intensive operation
-		slowrequests = slowrequests - 1
-		fmt.Println("Slowdown pattern active: ", slowrequests)
-	}
-
-	if today.Hour() == 10 { // trigger error pattern between 11 and 12 every day
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Something bad happened!"))
-	} else if errorrequests > 0 {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Something bad happened!"))
-		errorrequests = errorrequests - 1
-		fmt.Println("Error pattern active: ", errorrequests)
-	} else {
-		message := r.URL.Path
-		message = strings.TrimPrefix(message, "/")
-		message = "Hello " + message
-		w.Write([]byte(message))
-	}
+	message := r.URL.Path
+	message = strings.TrimPrefix(message, "/")
+	message = "Hello " + message
+	w.Write([]byte(message))
+	//fmt.Printf("GET / request processed.")
 	defer r.Body.Close()
 }
 
 func main() {
 	http.HandleFunc("/", sayHello)
+	http.HandleFunc("/config", receiveConfig)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
