d0392f2c58d9ac77c94f12a3ed354b3181403d2e
wolfgangB33r <wolfgang@smartlab.at>
2019-12-05 10:57:22 +0100

    changend balancer incident script

--- examples/balancer/trigger_incident.sh
+++ examples/balancer/trigger_incident.sh
@@ -1,12 +1,41 @@
+# first, configure the balancer to only send to 2 workers instead of previously 5
+
 curl -i -X POST \
    -H "Content-Type:application/json" \
    -d \
 '{
-  "ResourceConfig" : {
-    "Severity" : 100,
-    "Count" : 30000
+  "Callees" : [
+    { "Adr" : "http://localhost:9400", "Count" : 3 },
+    { "Adr" : "http://localhost:9500", "Count" : 3 }
+  ],
+  "Balanced" : true
+}' \
+ 'http://localhost:9000/config'
+
+# then, slowdown the remaining 2 workers because of the load shift
+
+curl -i -X POST \
+   -H "Content-Type:application/json" \
+   -d \
+'{
+  "SlowdownConfig" : {
+    "SlowdownMillis" : 500,
+    "Count" : 3000
   },
   "Callees" : [
   ]
 }' \
- 'http://localhost:8006/config'
+ 'http://localhost:9400/config'
+
+curl -i -X POST \
+   -H "Content-Type:application/json" \
+   -d \
+'{
+  "SlowdownConfig" : {
+    "SlowdownMillis" : 500,
+    "Count" : 3000
+  },
+  "Callees" : [
+  ]
+}' \
+ 'http://localhost:9500/config'
