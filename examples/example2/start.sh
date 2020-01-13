4a1b4531639ef649f58b4b7ab9eb8bc2ba763bf3
wolfgangB33r <wolfgang@smartlab.at>
2020-01-13 10:52:15 +0100

    added example2

---
+++ examples/example2/start.sh
@@ -0,0 +1,64 @@
+pkill stockweb 
+pkill stockticker
+pkill authenticationservice
+pkill contentprovider
+pkill database 
+
+sleep 5
+
+cp ../../demoservice stockweb
+cp ../../demoservice stockticker
+cp ../../demoservice authenticationservice
+cp ../../demoservice contentprovider
+cp ../../demoservice database
+
+./stockweb 9301 > stockweb.log 2>&1 &
+./stockticker 9302 > stockticker.log 2>&1 &
+./authenticationservice 9303 > authenticationservice.log 2>&1 &
+./contentprovider 9304 > contentprovider.log 2>&1 &
+./database 9305 > database.log 2>&1 &
+
+sleep 5
+
+curl -i -X POST \
+   -H "Content-Type:application/json" \
+   -d \
+'{
+  "Callees" : [
+    { "Adr" : "http://localhost:9303", "Count" : 1 },
+    { "Adr" : "http://localhost:9302", "Count" : 2 },
+    { "Adr" : "http://www.example.com", "Count" : 1 }
+  ]
+}' \
+ 'http://localhost:9301/config'
+
+curl -i -X POST \
+   -H "Content-Type:application/json" \
+   -d \
+'{
+  "Callees" : [
+    { "Adr" : "http://localhost:9303", "Count" : 2 },
+    { "Adr" : "http://localhost:9304", "Count" : 2 }
+  ]
+}' \
+ 'http://localhost:9302/config'
+
+curl -i -X POST \
+   -H "Content-Type:application/json" \
+   -d \
+'{
+  "Callees" : [
+    { "Adr" : "http://localhost:9305", "Count" : 5 }
+  ]
+}' \
+ 'http://localhost:9303/config'
+
+curl -i -X POST \
+   -H "Content-Type:application/json" \
+   -d \
+'{
+  "Callees" : [
+    { "Adr" : "http://localhost:9305", "Count" : 5 }
+  ]
+}' \
+ 'http://localhost:9304/config'
