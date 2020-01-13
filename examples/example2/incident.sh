48c6c092e499cb5d2a8b52a07b5eec3b4c15783c
wolfgangB33r <wolfgang@smartlab.at>
2020-01-13 15:36:45 +0100

    added incident script to example2

---
+++ examples/example2/incident.sh
@@ -0,0 +1,13 @@
+echo Triggering a slowdown of the database service by 50ms
+curl -i -X POST \
+   -H "Content-Type:application/json" \
+   -d \
+'{
+  "SlowdownConfig" : {
+    "SlowdownMillis" : 50,
+    "Count" : 3000
+  },
+  "Callees" : [
+  ]
+}' \
+ 'http://localhost:9305/config'
