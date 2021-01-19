47796e62f923b7f09c7f3e1256ce40a59c515fbb
Wolfgang Beer <wolfgang@smartlab.at>
2021-01-19 12:35:05 +0100

    modified log messages and forward Dynatrace trace header

--- service.go
+++ service.go
@@ -60,13 +60,13 @@ func receiveConfig(w http.ResponseWriter, r *http.Request) {
 		//fmt.Printf(string(body))
 		err = json.Unmarshal(body, &conf)
 		if err != nil {
-			fmt.Printf("Config payload wrong")
+			fmt.Printf("config payload wrong")
 			log.Println("config payload is wrong")
 			panic(err)
 		}
-		log.Println("received a new service config")
+		log.Println("received a new service config deployment")
 	default:
-		fmt.Fprintf(w, "Sorry, only POST method is supported.")
+		fmt.Fprintf(w, "sorry, only POST method is supported.")
 	}
 	defer r.Body.Close()
 }
@@ -76,24 +76,37 @@ func handleIcon(w http.ResponseWriter, r *http.Request) {
 }
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
-	log.Println("received a new service call")
 	reqcount++
-	fmt.Fprintf(w, "What I did %d:\n", reqcount)
+	fmt.Fprintf(w, "it's the %d call\n", reqcount)
+	fmt.Fprintf(w, "what I did:\n")
 	// first call all callees we have in the config with the multiplicity given
 	failures := false
+
+	traceheader := r.Header.Get("X-Dynatrace")
 
 	for ci, element := range conf.Callees {
 		if !conf.Balanced || reqcount%len(conf.Callees) == ci {
 			for i := 0; i < element.Count; i++ {
-				res, err := http.Get(element.Adr)
-				if err == nil {
-					defer res.Body.Close()
-					if res.StatusCode != 200 {
+				req, err := http.NewRequest("GET", element.Adr, nil)
+				if err != nil {
+					log.Fatal("error reading request. ", err)
+				}
+				req.Header.Set("X-Dynatrace", traceheader)
+				req.Header.Set("Cache-Control", "no-cache")
+
+				client := &http.Client{Timeout: time.Second * 10}
+
+				resp, err := client.Do(req)
+				if err != nil {
+					log.Fatal("error reading response. ", err)
+				} else {
+					if resp.StatusCode != 200 {
 						failures = true
 					}
 				}
+				defer resp.Body.Close()
 			}
-			fmt.Fprintf(w, "Called %s %d times\n", element.Adr, element.Count)
+			fmt.Fprintf(w, "called %s %d times\n", element.Adr, element.Count)
 		}
 	}
 	// then check if we should crash the process
@@ -106,8 +119,7 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 	if conf.SlowdownConfig.SlowdownMillis != 0 && conf.SlowdownConfig.Count > 0 {
 		time.Sleep(time.Duration(conf.SlowdownConfig.SlowdownMillis) * time.Millisecond)
 		conf.SlowdownConfig.Count = conf.SlowdownConfig.Count - 1
-		fmt.Fprintf(w, "Sleeped for %d millis\n", conf.SlowdownConfig.SlowdownMillis)
-		log.Printf("warning: slow service call: %d ms\n", conf.SlowdownConfig.SlowdownMillis)
+		fmt.Fprintf(w, "sleeped for %d millis\n", conf.SlowdownConfig.SlowdownMillis)
 	}
 	// then check if we should increase resource consumption
 	if conf.ResourceConfig.Severity != 0 && conf.ResourceConfig.Count > 0 {
@@ -119,7 +131,7 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 				}
 			}
 		}
-		fmt.Fprintf(w, "Allocated %d 100x100 matrices with random values\n", conf.ResourceConfig.Severity)
+		fmt.Fprintf(w, "allocated %d 100x100 matrices with random values\n", conf.ResourceConfig.Severity)
 		conf.ResourceConfig.Count = conf.ResourceConfig.Count - 1
 		log.Println("high resource consumption service call")
 	}
@@ -132,13 +144,12 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte("500 - Something bad happened!"))
 		}
-		fmt.Fprintf(w, "Returned an error response code\n")
+		fmt.Fprintf(w, "returned an error response code\n")
 		conf.ErrorConfig.Count = conf.ErrorConfig.Count - 1
-		log.Println("failed service call")
 	} else {
 		message := r.URL.Path
 		message = strings.TrimPrefix(message, "/")
-		message = "Finally returned " + message
+		message = "finally returned " + message
 		w.Write([]byte(message))
 	}
 	defer r.Body.Close()
